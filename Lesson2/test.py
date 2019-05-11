193650bcc9cfbb67bb7e268ca21f4b454b6ee969
MARS <wangyunfan@vtstar.net>
2019-05-11 23:59:23 +0800

    fea: py字符串与变量...

---
+++ Lesson2/test.py
@@ -0,0 +1,34 @@
+# 变量与字符串
+a = 1
+b = 2
+print(a + b)
+
+print(a * b)
+
+print(a / b)
+
+print(a - b)
+
+s1 = 'mars'
+s2 = 'wong'
+
+print(s1, s2)
+
+# 字符函数
+msg = 'HELLO world!'
+
+print(msg)
+print(msg.title())
+print(msg.upper())
+print(msg.lower())
+
+# 转义字符
+msg = 'I can: \n\tPython\n\tJavaScript'
+print(msg)
+
+# 删除空白
+massage = '  Mars is NB   '
+print('|' + massage + '|')
+print('|' + massage.rstrip() + '|')
+print('|' + massage.lstrip() + '|')
+print('|' + massage.strip() + '|')
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Python3 Study
 
+## 进如命令行模式
+
+```bash
+py -V
+# 查看python版本
+
+python
+# 进入命令行模式
+```
